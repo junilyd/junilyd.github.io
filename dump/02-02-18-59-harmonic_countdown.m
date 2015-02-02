da23ac2f8488310cf9c184cbbf11ef273a81b34b
junilyd <junilyd@gmail.com>
2015-02-02 18:59:18 +0100

    Auto Commit of 02-02-18-59-harmonic_countdown.m

---
+++ dump/02-02-18-59-harmonic_countdown.m
@@ -0,0 +1,20 @@
+fs = 44100;
+L = 12;
+l = 1:L;
+f0 = 200;
+w0 = 2*pi*f0;
+
+dur = 1;
+t=1/fs:1/fs:dur;
+
+sig = sin(w0*t);
+
+figure(1)
+plot(t, sig)
+
+
+x = zeros(fs*L);
+for i=12:-1:1
+    x(fs*i:end) = x + sin(w0*i*(1/fs:1/fs:(i-1)));
+end
+
