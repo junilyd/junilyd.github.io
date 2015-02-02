9119a4dcdda7968075f2b7750529b82477380da0
junilyd <junilyd@gmail.com>
2015-02-02 19:18:23 +0100

    Auto Commit of 02-02-19-18-harmonic_countdown.m

---
+++ dump/02-02-19-18-harmonic_countdown.m
@@ -0,0 +1,28 @@
+fs = 44100;
+L = 20;
+l = 1:L;
+f0 = 500;
+w0 = 2*pi*f0;
+
+dur = L;
+t=1/fs:1/fs:dur;
+
+
+x = zeros(fs*L,1);
+n=1;
+for i=L:-1:1
+    x = x + [zeros(fs*n,1); sin(w0*i*(1/fs:1/fs:(i-1)))'];
+    n=n+1;
+end
+
+longSig = [x; x(end-fs:end)]
+
+
+
+x=x/max(x);
+sound(x,fs)
+
+figure(1)
+subplot(211); plot(t,x);
+subplot(212); specgram(x);
+
