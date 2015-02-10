943433ee949693d314f83d5e2bc70c610bc1db07
junilyd <junilyd@gmail.com>
2015-02-10 11:33:07 +0100

    Auto Commit of 02-10-11-33-startup.m

---
+++ dump/02-10-11-33-startup.m
@@ -0,0 +1,85 @@
+%% Startup file to load paths
+
+addpath /home/hp8760w/Documents/uni/p8/project8/smc_lib
+addpath /home/hp8760w/Documents/uni/p8/project8/tuner_stuff
+addpath /home/hp8760w/Documents/uni/P7/project7/code/matlab/testfiles
+
+
+addpath /home/hp8760w/Documents/MATLAB/stoica/ch2
+addpath /home/hp8760w/Documents/MATLAB/stoica/ch3
+addpath /home/hp8760w/Documents/MATLAB/stoica/ch4
+addpath /home/hp8760w/Documents/MATLAB/stoica/ch5
+addpath /home/hp8760w/Documents/MATLAB/stoica/ch6
+addpath /home/hp8760w/Documents/MATLAB/stoica/data
+addpath /home/hp8760w/Documents/MATLAB/stoica/extras
+
+addpath /home/hp8760w/Documents/uni/P7/courses/sound_processing/matlab
+
+addpath /home/hp8760w/Documents/uni/P7/project7/code/matlab
+
+addpath /home/hp8760w/Documents/MATLAB/matlab2tikz/tools/
+
+addpath /home/hp8760w/Documents/MATLAB/matlab2tikz/src/
+
+addpath /home/hp8760w/Documents/MATLAB/SSUM/
+
+addpath /home/hp8760w/Documents/MATLAB/cvx/
+
+addpath /home/hp8760w/Documents/MATLAB/Fisher4Cast/
+
+addpath /home/hp8760w/Documents/MATLAB/pitch/
+
+addpath /home/hp8760w/Documents/MATLAB/pitch/pitch/
+
+addpath /home/hp8760w/Documents/MATLAB/mspr/
+
+addpath /home/hp8760w/Documents/MATLAB/atlast65/
+
+addpath /home/hp8760w/Documents/MATLAB/laydata/
+
+addpath /home/hp8760w/Documents/MATLAB/plots/
+
+addpath /home/hp8760w/Documents/MATLAB/prtools/
+addpath /home/hp8760w/Documents/MATLAB/prtools/additional_files2/
+addpath /home/hp8760w/Documents/MATLAB/prtools/prdatasets/
+addpath /home/hp8760w/Documents/MATLAB/prtools/prdatafiles/
+
+addpath /home/hp8760w/Documents/uni/P7/project7/courses/mspr_2013/matlab/organ/
+
+addpath /home/hp8760w/Documents/uni/P7/project7/courses/mspr/m-filer 
+
+addpath /home/hp8760w/Documents/MATLAB/sp
+
+addpath /home/hp8760w/Documents/MATLAB/graphwar
+
+addpath /home/hp8760w/Documents/MATLAB/wl
+
+addpath /home/hp8760w/Documents/MATLAB/network
+
+addpath /home/hp8760w/Documents/MATLAB/lin_alg
+
+addpath /home/hp8760w/Documents/MATLAB/mcop
+
+addpath /home/hp8760w/Documents/MATLAB/export_fig
+
+addpath /home/hp8760w/Documents/MATLAB/math
+
+addpath /home/hp8760w/Documents/MATLAB/bf
+
+addpath /home/hp8760w/Documents/MATLAB/miditoolbox
+addpath /home/hp8760w/Documents/MATLAB/miditoolbox/midi_lib
+javaaddpath /home/hp8760w/Documents/MATLAB/miditoolbox/KaraokeMidiJava.jar
+
+addpath /home/hp8760w/Documents/uni/P7/project7/courses/music_preception/MPC_jacob/m-files 
+
+addpath /home/hp8760w/Documents/MATLAB/audio
+
+addpath /home/hp8760w/Documents/MATLAB/prst
+
+addpath /home/hp8760w/Documents/MATLAB/project
+
+addpath /home/hp8760w/Documents/MATLAB/opti
+
+addpath /home/hp8760w/Documents/MATLAB/comm
+
+format compact;
